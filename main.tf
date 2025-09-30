929836764efc0de4c94677928db2d457bdd40726
JagadeeshDeviPushkar <101493807+JagadeeshDeviPushkar@users.noreply.github.com>
2025-10-01 01:35:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-region = "us-east-1"
+region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-ami = "ami-0360c520857e3138f"
+ami = "ami-01b6d88af12965bb6"
 instance_type = "t2.micro"
 tags = {
 Name = "hcp-server"
